cc465a68e0f6a4ccb07102fe275c985e0f52e548
JohnGolgota <js684new@gmail.com>
2023-12-09 22:03:06 -0500

    robar es mi pasion

--- TypeScript/uno/resp/AddTwoNumbers.ts
+++ TypeScript/uno/resp/AddTwoNumbers.ts
@@ -25,5 +25,36 @@ function addTwoNumbers(l1: ListNode | null | undefined, l2: ListNode | null | un
     return result.next;
 }
 
+function addTwoNumbers2(l1: ListNode | null | undefined, l2: ListNode | null | undefined): ListNode | null {
+    const head = new ListNode(0)
+    let tail = head
+    let reminder = 0 as number
+    while (l1 !== null || l2 !== null) {
+        let s1 = 0, s2 = 0;
+        if (l1) {
+            s1 = l1.val
+            l1 = l1.next
+        }
+        if (l2) {
+            s2 = l2.val
+            l2 = l2.next
+        }
+
+        const sum = s2 + s1 + reminder
+        const newNode = new ListNode(sum % 10)
+        tail.next = newNode
+        tail = tail.next
+        if (sum >= 10) {
+            reminder = 1
+        } else {
+            reminder = 0
+        }
+    }
+    if (reminder === 1) {
+        tail.next = new ListNode(1)
+    }
+    return head.next
+}
+
 export { ListNode, addTwoNumbers };
 
