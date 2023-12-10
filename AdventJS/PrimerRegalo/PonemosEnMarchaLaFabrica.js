f4c16dc9cb5ac17f887e31652db6bf5c28727338
JohnGolgota <js684new@gmail.com>
2023-12-10 15:26:07 -0500

    Dia 2 Me vo a poner al día

---
+++ AdventJS/PrimerRegalo/PonemosEnMarchaLaFabrica.js
@@ -0,0 +1,48 @@
+// En el taller de Santa, los elfos tienen una lista de regalos que desean fabricar y un conjunto limitado de materiales.
+
+// Los regalos son cadenas de texto y los materiales son caracteres.Tu tarea es escribir una función que, dada una lista de regalos y los materiales disponibles, devuelva una lista de los regalos que se pueden fabricar.
+
+// Un regalo se puede fabricar si contamos con todos los materiales necesarios para fabricarlo.
+
+const gifts = ['tren', 'oso', 'pelota']
+const materials = 'tronesa'
+
+// manufacture(gifts, materials) // ["tren", "oso"]
+// // 'tren' SÍ porque sus letras están en 'tronesa'
+// // 'oso' SÍ porque sus letras están en 'tronesa'
+// // 'pelota' NO porque sus letras NO están en 'tronesa'
+
+// const gifts = ['juego', 'puzzle']
+// const materials = 'jlepuz'
+
+// manufacture(gifts, materials) // ["puzzle"]
+
+// const gifts = ['libro', 'ps5']
+// const materials = 'psli'
+
+// manufacture(gifts, materials) // []
+
+function manufacture(gifts, materials) {
+  const giftsArray = gifts.map(gift => gift.split(''))
+  const materialsArray = materials.split('')
+
+  const result = giftsArray.map(gift => {
+    const giftCopy = [...gift]
+    const giftMaterials = giftCopy.filter(letter => {
+      const index = materialsArray.indexOf(letter)
+      if (index !== -1) {
+        return true
+      }
+      return false
+    })
+    if (giftMaterials.length === gift.length) {
+      return gift.join('')
+    }
+  })
+
+  return result.filter(gift => gift)
+}
+
+console.log(manufacture(gifts, materials))
+
+export default manufacture
