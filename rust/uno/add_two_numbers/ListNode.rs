e33ece95b081955e4aa21a9e09819c5f64521e48
JohnGolgota <js684new@gmail.com>
2023-12-09 23:18:53 -0500

    Me gusta rust

---
+++ rust/uno/add_two_numbers/ListNode.rs
@@ -0,0 +1,10 @@
+pub struct ListNode {
+    pub val: i32,
+    pub next: Option<Box<ListNode>>,
+}
+
+impl ListNode {
+    pub fn new(val: i32) -> Self {
+        ListNode { val, next: None }
+    }
+}
