188c8fec4001860db34bdad673f03d87312ee053
JohnGolgota <js684new@gmail.com>
2023-12-14 04:56:17 -0500

    No lo entiendo Why loco Why?!!

--- AdventJS/PrimerRegalo/LosRenosAPrueba.ts
+++ AdventJS/PrimerRegalo/LosRenosAPrueba.ts
@@ -3,10 +3,10 @@ const result = maxDistance(movements)
 result === 2 ? console.log('Test passed') : console.log('Test failed')
 console.log(result) // -> 2
 
-const movements2 = '<<<>'
-const result2 = maxDistance(movements2)
-result2 === 2 ? console.log('Test passed') : console.log('Test failed')
-console.log(result2) // -> 2
+// const movements2 = '<<<>'
+// const result2 = maxDistance(movements2)
+// result2 === 2 ? console.log('Test passed') : console.log('Test failed')
+// console.log(result2) // -> 2
 
 // const movements3 = '>***>'
 // const result3 = maxDistance(movements3)
@@ -15,26 +15,38 @@ console.log(result2) // -> 2
 
 // const movements4 = '****<>>>><*****'
 // const result4 = maxDistance(movements4)
-// result4 === 11 ? console.log('Test passed') : console.log('Test failed')
+// result4 === 12 ? console.log('Test passed') : console.log('Test failed')
 // console.log(result4) // -> 4
+
+// const movements5 = '*<*'
+// const result5 = maxDistance(movements5)
+// result5 === 1 ? console.log('Test passed') : console.log('Test failed')
+// console.log(result5) // -> 1
+
+const movements6 = '*>><<*'
+const result6 = maxDistance(movements6)
+result6 === 2 ? console.log('Test passed') : console.log('Test failed')
+console.log(result6) // -> 2
+
 function maxDistance(movements: string) {
   let disatace = 0
   let derecha = 0
   let izquierda = 0
-  let dual = 0
   for (let i = 0; i < movements.length; i++) {
     // Contar cuantos > y < hay
+    console.log(movements[i])
     if (movements[i] === '>') {
       derecha++
+      disatace++
     }
     if (movements[i] === '<') {
       izquierda++
+      disatace--
     }
     if (movements[i] === '*') {
-      dual++
+      derecha >= izquierda ? disatace++ : disatace--
     }
   }
-  disatace = derecha > izquierda ? dual + izquierda - derecha : dual + derecha - izquierda
-  let result = Math.abs(disatace)
-  return result
+  const maxDistance = Math.abs(disatace)
+  return maxDistance
 }
